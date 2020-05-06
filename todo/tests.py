d6d45d8f2e613cb588488637e89c94e9d3d69c2f
lw <lw_git@mail.ru>
2020-05-06 10:15:52 +0500

    Added TaskModelTests

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Task
 
-# Create your tests here.
+
+class TaskModelTests(TestCase):
+
+    def test_create_task(self):
+        self.assertEqual(Task.objects.count(), 0)
+        task = Task.objects.create(title='test')
+
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(task.title, 'test')
+        self.assertFalse(task.completed)
